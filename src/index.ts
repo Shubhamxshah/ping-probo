72aec325dec55097d8f78e46364592b3c40ce19d
Shubhamxshah <ritikgujarat@gmail.com>
2025-06-08 00:30:43 +0530

    modified

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 import axios from "axios";
 import dotenv from "dotenv";
 
-dotenv.config(); 
+dotenv.config();
 
+const BACKEND_URL = process.env.BACKEND_URL;
 async function ping() {
-  const BACKEND_URL = process.env.BACKEND_URL
 
-  setTimeout(async () => {
-    await axios.post(`${BACKEND_URL}/api/v1/trade/sell`, {
-      userId: process.env.USER_ID,
-      noOfTokens: 3,
-      event: "btc",
-      type: "YES",
-      price: "700",
-    });
-  }, 200);
+  await axios.post(`${BACKEND_URL}/api/v1/trade/sell`, {
+    userId: process.env.USER_ID,
+    noOfTokens: 3,
+    event: "btc",
+    type: "YES",
+    price: "700",
+  });
 
   setTimeout(async () => {
     await axios.post(`${BACKEND_URL}/api/v1/trade/sell`, {
@@ -34,7 +32,7 @@ async function ping() {
       type: "YES",
       price: "800",
     });
-  }, 200);
+  }, 400);
 
   setTimeout(async () => {
     await axios.post(`${BACKEND_URL}/api/v1/trade/sell`, {
@@ -44,7 +42,7 @@ async function ping() {
       type: "YES",
       price: "850",
     });
-  }, 200);
+  }, 600);
 
   setTimeout(async () => {
     await axios.post(`${BACKEND_URL}/api/v1/trade/sell`, {
@@ -54,7 +52,7 @@ async function ping() {
       type: "YES",
       price: "900",
     });
-  }, 200);
+  }, 800);
 
   setTimeout(async () => {
     await axios.post(`${BACKEND_URL}/api/v1/trade/sell`, {
@@ -64,7 +62,7 @@ async function ping() {
       type: "NO",
       price: "750",
     });
-  }, 200);
+  }, 1000);
 
   setTimeout(async () => {
     await axios.post(`${BACKEND_URL}/api/v1/trade/sell`, {
@@ -74,7 +72,7 @@ async function ping() {
       type: "NO",
       price: "800",
     });
-  }, 200);
+  }, 1200);
 
   setTimeout(async () => {
     await axios.post(`${BACKEND_URL}/api/v1/trade/sell`, {
@@ -84,7 +82,7 @@ async function ping() {
       type: "NO",
       price: "850",
     });
-  }, 200);
+  }, 1400);
 
   setTimeout(async () => {
     await axios.post(`${BACKEND_URL}/api/v1/trade/sell`, {
@@ -94,7 +92,7 @@ async function ping() {
       type: "NO",
       price: "900",
     });
-  }, 200);
+  }, 1600);
 
   setTimeout(async () => {
     await axios.post(`${BACKEND_URL}/api/v1/trade/sell`, {
@@ -104,17 +102,13 @@ async function ping() {
       type: "NO",
       price: "950",
     });
-  }, 200);
-
-  setTimeout(async () => {
-    await axios.post(`${BACKEND_URL}/api/v1/balance/mint`, {
-      userId: process.env.USER_ID,
-      event: "btc",
-      noOfTokens: 20,
-    });
-  });
+  }, 1800);
 }
 
 setInterval(() => {
-  ping()
-}, 3000)
+  ping();
+}, 2000);
+
+setTimeout(async () => {
+  await axios.post(`${BACKEND_URL}/api/v1/user/reset`)
+}, 30*60*1000)
